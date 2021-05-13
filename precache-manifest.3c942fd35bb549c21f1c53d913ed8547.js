0517bc67bb17f4e71366aa442f21047e629f600b
ravichadda <ravi.chadda@tcs.com>
2021-05-13 11:56:34 +0530

    Updates

---
+++ precache-manifest.3c942fd35bb549c21f1c53d913ed8547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be1beb306083d250934451224ef1473",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0dba6e711965c134bf89",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "f0a38136ecaf0c47582b",
+    "url": "./static/js/2.328dfcb5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.328dfcb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dba6e711965c134bf89",
+    "url": "./static/js/main.7e4c9a28.chunk.js"
+  },
+  {
+    "revision": "76d940f5ab45da2b214e",
+    "url": "./static/js/runtime-main.ac90005d.js"
+  }
+]);
